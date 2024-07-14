Atom-X-Devs/android_kernel_xiaomi_sm7325

drivers: iommu: import XIAOMI changes
Change-Id: Icff38b58868253b36f5d6c8ce57da92621b139f6
Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>

drivers/iommu/arm-smmu.c
@@ -3299,6 +3299,9 @@ static size_t arm_smmu_unmap(struct iommu_domain *domain, unsigned long iova,
 	arm_smmu_rpm_get(smmu);
 	spin_lock_irqsave(&smmu_domain->cb_lock, flags);
 	ret = ops->unmap(ops, iova, size, gather);
+#ifdef CONFIG_MACH_XIAOMI
+	arm_smmu_deferred_flush(smmu_domain);
+#endif
 	spin_unlock_irqrestore(&smmu_domain->cb_lock, flags);
 	arm_smmu_rpm_put(smmu);
 
@@ -3416,6 +3419,9 @@ static size_t arm_smmu_map_sg(struct iommu_domain *domain, unsigned long iova,
 			spin_lock_irqsave(&smmu_domain->cb_lock, flags);
 			list_splice_init(&nonsecure_pool,
 					 &smmu_domain->nonsecure_pool);
+#ifdef CONFIG_MACH_XIAOMI
+			arm_smmu_deferred_flush(smmu_domain);
+#endif
 			ret = pgtbl_info->map_sg(ops, iova, sg_start,
 						 idx_end - idx_start, prot,
 						 &size);

EndCredits/android_kernel_xiaomi_sm8350-miui

Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

drivers/iommu/arm-smmu.c
@@ -3299,6 +3299,9 @@ static size_t arm_smmu_unmap(struct iommu_domain *domain, unsigned long iova,
 	arm_smmu_rpm_get(smmu);
 	spin_lock_irqsave(&smmu_domain->cb_lock, flags);
 	ret = ops->unmap(ops, iova, size, gather);
+#ifdef CONFIG_BAORD_XIAOMI
+	arm_smmu_deferred_flush(smmu_domain);
+#endif
 	spin_unlock_irqrestore(&smmu_domain->cb_lock, flags);
 	arm_smmu_rpm_put(smmu);
 
@@ -3416,6 +3419,9 @@ static size_t arm_smmu_map_sg(struct iommu_domain *domain, unsigned long iova,
 			spin_lock_irqsave(&smmu_domain->cb_lock, flags);
 			list_splice_init(&nonsecure_pool,
 					 &smmu_domain->nonsecure_pool);
+#ifdef CONFIG_BAORD_XIAOMI
+			arm_smmu_deferred_flush(smmu_domain);
+#endif
 			ret = pgtbl_info->map_sg(ops, iova, sg_start,
 						 idx_end - idx_start, prot,
 						 &size);